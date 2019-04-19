62cf6147e0136cde005353e6274a56e5b01615fd
baiqiang <xia.research@gmail.com>
2019-04-19 17:32:39 +0300

    put code and notebook folders

---
+++ ds/doublyLinkedList.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+## Baiqiang XIA implementation of data structures
+
+# doubly linked list
+# implemented as seperated classes: node and list
+class dll_node(object):
+    def __init__(self, val, pre = None, nxt = None):
+        self.data = val
+        self.prev = pre
+        self.next = nxt
+        
+class doublyLinkedList(object):
+    ## initialization
+    def __init__(self, head = None, tail = None):
+        self.head = head
+        self.tail = tail
+        
+    ## print dll
+    def printDLL(self):
+        print('@print dll from head!')
+        
+        ## print root label
+        if self.head:
+            print('(root)', end = '')
+        else:
+            print('@print empty dll!')
+        
+        ## print nodes  
+        cur = self.head
+        cnt = 0
+        while cur:
+            if cur.next:
+                print(cur.data, end = ' <==>')
+            else:
+                print(cur.data)
+            cur = cur.next
+            cnt += 1
+            
+        ## end printing
+        print('@finished printing %d nodes of dll!'%cnt)
+        return 1
+    
+    ## insert at head of dll
+    def insert_at_head(self, val):    
+        ## make dll node
+        node = dll_node(val)        
+        ## if dll is empty
+        if not self.head:
+            self.head = node
+            self.tail = node
+            print('@insert node to empty dll!')
+            return 1
+        ## if dll is not empty
+        else:
+            self.head.prev = node
+            node.next = self.head
+            self.head = node
+            print('@insert node at head of dll!')
+            return 1
+        
+        ## nothing happens here, just a habit
+        return -1
+        
+    ## search node in dll
+    def search(self, val):
+        if not self.head:
+            print('@dll is empty, can not search!')
+        else:
+            cur = self.head
+            while cur: ## loop till the tail.next
+                if cur.data == val:
+                    print('@found node with value %d in dll!'%val)
+                    return 1
+                cur = cur.next
+        print('@no node found with value %d!'%val)
+        return -1
+        
+    ## delete node in dll
+    def delete(self, val):
+        ## empty dll
+        if not self.head:
+            print('@not able to delete node in empty dll!')
+            return 1
+        ## 1 node dll
+        elif self.head == self.tail:
+            if self.head.data == val:
+                self.head = None
+                self.tail = None
+                print('@delete 1 node with value %d, now dll is empty!'%val)
+                return 1
+            else:
+                print('@no matched node to delete!')
+        ## >= 2 nodes dll     
+        else:
+            ## if head node matches, move the head to the next node, set to None the prev of new head
+            if self.head.data == val:
+                self.head = self.head.next
+                self.head.prev = None
+                print('@delete 1 node with value %d at head in dll!'%val)
+                return 1
+            ## if head does not match
+            else:
+                pre = self.head
+                cur = self.head.next
+                while cur != self.tail: ## tail matches should be handled differently
+                    if cur.data == val:
+                        pre.next = cur.next
+                        cur.next.prev = cur.prev
+                        print('@delete 1 node with value %d inside of dll!'%val)
+                        return 1
+                    
+                    ## update pre, cur
+                    pre = cur
+                    cur = cur.next
+                
+                ## now cur points to self.tail
+                if cur.data == val:
+                    self.tail = cur.prev
+                    cur.prev.next = None
+                    print('@delete 1 node with value %d at tail of dll!'%val)
+                    return 1
+                    
+                ## no match to delete
+                else:
+                    print('@no matched node with value %d in dll to delete!'%val)
+                    return -1
+                
+                        
+                        
+                    
+            
+            
+            
---
+++ ds/singlyLinkedList.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+## Baiqiang XIA implementation of data structures
+
+# singly linked list
+# implemented as seperated classes: node and list
+class sll_node(object):
+    def __init__(self, val, nxt = None):
+        self.data = val
+        self.next = nxt
+       
+class singlyLinkedList(object):
+
+    ## initialization
+    def __init__(self, head = None):
+        self.head = head
+        
+    ## print sll
+    def printSLL(self):
+        cur = self.head
+        cnt = 0
+        print('(head)', end = '')
+        while cur:
+            print(cur.data, end = ' -->')
+            cur = cur.next
+            cnt += 1
+        print('\n@finished printing sll of %d nodes'%cnt)
+        return 1
+    
+    ## insert at tail of sll
+    def insert_at_tail(self, val):
+        ## when sll is empty
+        if not self.head:
+            self.head = sll_node(val)
+            print('@insert at tail to empty sll!')
+            return 1
+        ## when sll is not empty
+        else:
+            cur = self.head
+            while cur.next:
+                cur = cur.next ## until cur.next is None, but cur is not None
+            ## now pointer is at the tail, put added node as tail.next
+            cur.next = sll_node(val)
+            print('@insert at tail of sll!')
+            return 1
+            
+    ## insert at head of sll
+    def insert_at_head(self, val):
+            self.head = sll_node(val, None) if not self.head else sll_node(val, self.head)
+            print('@insert at head of sll!')
+            return 1
+            # long version
+            # if not self.head:
+                # self.head = sll_node(val)
+                # print('@insert node as head to empty sll!')
+                # return 1
+            # else:
+                # self.head = sll_node(val, self.head)
+                # print('@insert node as head of sll!')
+                # return 1
+                
+    ## search for a node with value in sll
+    def search(self, val):
+        cur = self.head
+        while cur:
+            if cur.data == val:
+                print('@value %d found in sll!'%val)
+                return 1
+            cur = cur.next
+        print('@value %d not found in sll!'%val)
+        return -1
+    
+    ## delete a node with value
+    def delete(self, val):
+        ## if sll is empty
+        if not self.head:
+            print('@sll is empty, no node to delete!')
+            return 1
+        ## if sll head is the match
+        elif self.head.data == val:
+                self.head = self.head.next
+                print('@delete at sll head!')
+                return 1
+        ## if sll head is not the match
+        else:
+            cur = self.head
+            nxt = self.head.next
+            while nxt:
+                if nxt.data == val: ## if find match at next
+                    cur.next = nxt.next ## link to the next next 
+                    print('@delete node inside sll!')
+                    return 1
+                cur = nxt
+                nxt = cur.next
+        ## no found
+        print('@deletion failed, no matched node found!')
+        return -1
+        
+    ## reverse sll
+    def reverse(self):
+        ## for empty sll
+        if not self.head:
+            print('@sll is empty!')
+            return 1
+        ## for single node sll
+        elif not self.head.next:
+            print('@sll has only 1 node, no need to reverse!')
+            return 1
+        ## for multi nodes sll
+        else:
+            pre = None
+            cur = self.head
+            while cur:
+                nxt = cur.next ## buffer the cur.next
+                cur.next = pre ## change cur.next to pre
+                pre = cur ## move forward 1 node
+                cur = nxt ## move forward 1 node
+            ## at the end of loop, cur is None,  pre is the last valid node
+            self.head = pre
+            print('@finished reversing sll!')
+            return 1
+            
